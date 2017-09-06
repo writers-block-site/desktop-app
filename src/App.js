ce8ef6ed9d8029d93ed38c1795249c5b9600636e
Douglass Hooker <DouglassHooker@gmail.com>
2017-09-05 20:30:11 -0500

    Removed filler stuff, starting basic layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
+import DocumentsContainer from './components/DocumentsContainer';
 import './App.css';
 
 class App extends Component {
+    componentWillMount(){
+
+    }
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>React</h2>
-                </div>
-                <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <DocumentsContainer />
             </div>
         );
     }
---
+++ src/components/DocumentsContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class DocumentsContainer extends Component{
+    render() {
+        return(
+            <div className='documents'>
+            </div>
+        );
+    }
+}
